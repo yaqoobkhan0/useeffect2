7eeb2021fcc6ba0d91ca45f44ec5998b6c7b6d50
Yaqoob khan <99115980+yaqoobkhan0@users.noreply.github.com>
2022-11-10 19:22:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import FindMovie from "./Components/FindMovie";
+
+function App() {
+  return (
+    <div className="App">
+      <FindMovie />
+    </div>
+  );
+}
+
+export default App;
